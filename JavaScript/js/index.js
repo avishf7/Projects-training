852f0efbf0223a9193b9c4f0284162e0d760d189
avishay farkash <62483176+avishf7@users.noreply.github.com>
2024-05-28 14:05:30 +0300

    To do list - JS add Drag functionality

--- JavaScript/js/index.js
+++ JavaScript/js/index.js
@@ -45,23 +45,71 @@ window.addEventListener('storage', () => {
     const listItems = []
 
     for (const task of tasksList) {
-        // Create the main list item element
+        // Create the main list item element and set it as draggable
         const liEl = document.createElement("li")
+        liEl.setAttribute("draggable", "true")
+
         // If the task is checked, add the "checked" class to the list item
         if (task.isChecked) liEl.classList.add("checked");
+        
         // Toggle the isChecked state and update localStorage when the list item is clicked
         liEl.addEventListener('click' , () => {
             task.isChecked = !task.isChecked
             localStorage.setItem("tasks", JSON.stringify(tasksList))
         })
+        
+        // Drag and drop event listeners
+        liEl.addEventListener('dragstart' , (ev) => {
+            liEl.classList.add('dragging')
+            ev.dataTransfer.effectAllowed = "move"
+        })
+
+        liEl.addEventListener('dragend', () => {
+            liEl.classList.remove('dragging')
+        })
+
+        liEl.addEventListener('dragover', ev => {
+            const draggable = document.querySelector('.dragging')
+
+            if (draggable !== liEl){
+
+                ev.preventDefault()
+                ev.dataTransfer.dropEffect = "move"
+                liEl.classList.add('hovered')
+            }
+
+        })
+        
+        liEl.addEventListener('dragleave', () => {
+            const draggable = document.querySelector('.dragging')
+
+            if (draggable !== liEl) liEl.classList.remove('hovered')
+        })
+        
+        liEl.addEventListener('drop', ev => {
+            ev.preventDefault()
+            const draggable = document.querySelector('.dragging')
+            const firstIndexToSwap = [...toDoListEl.children].indexOf(draggable)
+            const secondIndexToSwap = tasksList.indexOf(task)
+
+            ev.preventDefault()
+
+            const tmp = tasksList[firstIndexToSwap]
+            tasksList[firstIndexToSwap] = tasksList[secondIndexToSwap]
+            tasksList[secondIndexToSwap] = tmp
+            localStorage.setItem("tasks", JSON.stringify(tasksList))
+        })
+
               
         // Create a div to act as the container for the task description and delete button
         const divEl = document.createElement("div");
         divEl.classList.add("item");
 
+
         // Create a paragraph element for the task description
         const pEl = document.createElement("p");
         pEl.textContent = task.desc;
+
 
         // Create a button element for deleting the task
         const closeBtn = document.createElement("button");
