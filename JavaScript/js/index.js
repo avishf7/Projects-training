b9e33a429a11fcfba40a4f76f02c712e98a91319
avishay farkash <62483176+avishf7@users.noreply.github.com>
2024-05-29 15:38:54 +0300

    To do list - JS project extract event handlers and fix the grab functionality

--- JavaScript/js/index.js
+++ JavaScript/js/index.js
@@ -36,89 +36,24 @@ addBtn.addEventListener( 'click', () => {
 })
 
 
-// Update the task list UI when 'storage' event is triggered
+/**
+ * Update the task list UI when 'storage' event is triggered.
+ */
 window.addEventListener('storage', () => { 
 
     tasksList = JSON.parse(localStorage.getItem("tasks")) || []
 
-
     const listItems = []
 
     for (const task of tasksList) {
-        // Create the main list item element and set it as draggable
-        const liEl = document.createElement("li")
-        liEl.setAttribute("draggable", "true")
 
-        // If the task is checked, add the "checked" class to the list item
-        if (task.isChecked) liEl.classList.add("checked");
-        
-        // Toggle the isChecked state and update localStorage when the list item is clicked
-        liEl.addEventListener('click' , () => {
-            task.isChecked = !task.isChecked
-            localStorage.setItem("tasks", JSON.stringify(tasksList))
-        })
-        
-        // Drag and drop event listeners
-        liEl.addEventListener('dragstart' , (ev) => {
-            liEl.classList.add('dragging')
-            ev.dataTransfer.effectAllowed = "move"
-        })
+        const liEl = createTaskLi(task)
+     
+        const divEl = createTaskContainer()
 
-        liEl.addEventListener('dragend', () => {
-            liEl.classList.remove('dragging')
-        })
+        const pEl = createTaskParagraph(task)
 
-        liEl.addEventListener('dragover', ev => {
-            const draggable = document.querySelector('.dragging')
-
-            if (draggable !== liEl){
-
-                ev.preventDefault()
-                ev.dataTransfer.dropEffect = "move"
-                liEl.classList.add('hovered')
-            }
-
-        })
-        
-        liEl.addEventListener('dragleave', () => {
-            const draggable = document.querySelector('.dragging')
-
-            if (draggable !== liEl) liEl.classList.remove('hovered')
-        })
-        
-        liEl.addEventListener('drop', ev => {
-            ev.preventDefault()
-            const draggable = document.querySelector('.dragging')
-            const firstIndexToSwap = [...toDoListEl.children].indexOf(draggable)
-            const secondIndexToSwap = tasksList.indexOf(task)
-
-            ev.preventDefault()
-
-            const tmp = tasksList[firstIndexToSwap]
-            tasksList[firstIndexToSwap] = tasksList[secondIndexToSwap]
-            tasksList[secondIndexToSwap] = tmp
-            localStorage.setItem("tasks", JSON.stringify(tasksList))
-        })
-
-              
-        // Create a div to act as the container for the task description and delete button
-        const divEl = document.createElement("div");
-        divEl.classList.add("item");
-
-
-        // Create a paragraph element for the task description
-        const pEl = document.createElement("p");
-        pEl.textContent = task.desc;
-
-
-        // Create a button element for deleting the task
-        const closeBtn = document.createElement("button");
-        closeBtn.classList.add("delete-button");
-        closeBtn.textContent = '✕';
-        // Remove the task and update localStorage when the delete button is clicked
-        closeBtn.addEventListener('click', () => {
-        localStorage.setItem("tasks", JSON.stringify(tasksList.filter(t => t.desc !== task.desc)));
-        });
+        const closeBtn = createTaskDeleteButton(task)
         
 
         liEl.appendChild(divEl)
@@ -127,9 +62,143 @@ window.addEventListener('storage', () => {
         listItems.push(liEl)        
     } 
 
-    toDoListEl.replaceChildren(...listItems)
-
-    
+    toDoListEl.replaceChildren(...listItems)   
 })
 
+
+/**
+ * Create a delete button for a task.
+ */
+function createTaskDeleteButton(task) {
+    const closeBtn = document.createElement("button")
+    closeBtn.classList.add("delete-button")
+    closeBtn.textContent = '✕'
+    closeBtn.addEventListener('click', delBtnClickHandler(task))
+    return closeBtn
+}
+
+/**
+ * Creates a click handler for the delete button to remove a task.
+ */
+function delBtnClickHandler(task) {
+    return () => {
+        localStorage.setItem("tasks", JSON.stringify(tasksList.filter(t => t.desc !== task.desc)))
+    }
+}
+
+/**
+ * Create a paragraph element for the task description.
+ */
+function createTaskParagraph(task) {
+    const pEl = document.createElement("p")
+    pEl.textContent = task.desc
+    return pEl
+}
+
+/**
+ * Create a container div for the task description and delete button.
+ */
+function createTaskContainer() {
+    const divEl = document.createElement("div")
+    divEl.classList.add("item")
+    return divEl
+}
+
+/**
+ * Create the main list item element for a task.
+ */
+function createTaskLi(task) {
+    const liEl = document.createElement("li")
+
+    // Set the list item element as draggable
+    liEl.setAttribute("draggable", "true")
+
+    if (task.isChecked) liEl.classList.add("checked")
+
+    liEl.addEventListener('click', liClickHandler(task))
+
+    // Drag and drop event listeners
+    liEl.addEventListener('dragstart', liDragstartHandler(liEl));
+    liEl.addEventListener('dragend', liDragendHandler(liEl));
+    liEl.addEventListener('dragover', liDragoverHandler(liEl));
+    liEl.addEventListener('dragleave', liDragleaveHandler(liEl));
+    liEl.addEventListener('drop', liDropHandler(task));
+
+    return liEl
+}
+
+/**
+ * Creates a Handler for the 'drop' event to reorder tasks.
+ */
+function liDropHandler(task) {
+    return ev => {
+        const draggable = document.querySelector('.dragging')
+        const fromIndex = [...toDoListEl.children].indexOf(draggable)
+        const toIndex = tasksList.indexOf(task)
+
+        ev.preventDefault()
+
+        const taskToMove = tasksList.splice(fromIndex, 1)[0]
+        tasksList.splice(toIndex, 0, taskToMove)
+
+        localStorage.setItem("tasks", JSON.stringify(tasksList))
+    }
+}
+
+/**
+ * Creates a Handler for the 'dragleave' event to remove highlight.
+ */
+function liDragleaveHandler(liEl) {
+    return () => {
+        const draggable = document.querySelector('.dragging')
+
+        if (draggable !== liEl) liEl.classList.remove('hovered')
+    }
+}
+
+/**
+ * Creates a Handler for the 'dragover' event to allow dropping and highlight the target.
+ */
+function liDragoverHandler(liEl) {
+    return ev => {
+
+        if (!liEl.classList.contains('dragging')) {
+
+            ev.preventDefault()
+            ev.dataTransfer.dropEffect = "move"
+            liEl.classList.add('hovered')
+        }
+    }
+}
+
+/**
+ * Creates a Handler for the 'dragend' event to remove dragging class.
+ */
+function liDragendHandler(liEl) {
+    return () => {
+        liEl.classList.remove('dragging')
+    }
+}
+
+/**
+ * Creates a Handler for the 'dragstart' event to add dragging class.
+ */
+function liDragstartHandler(liEl) {
+    return (ev) => {
+        liEl.classList.add('dragging')
+        ev.dataTransfer.effectAllowed = "move"
+    }
+}
+
+/**
+ * Creates a Handler for the 'click' event to toggle task completion.
+ */
+function liClickHandler(task) {
+    return function () {
+        task.isChecked = !task.isChecked
+        localStorage.setItem("tasks", JSON.stringify(tasksList))
+    }
+}
+
 dispatchEvent(new Event("storage"))
+
